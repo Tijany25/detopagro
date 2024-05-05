9c4350e88bd7e2fef6b1f86abf5b60f223d96eb1
Tijany25 <92982320+Tijany25@users.noreply.github.com>
2024-05-05 21:00:49 +0100

    update

--- src/app/modules/views/admin/ytLinks/ytLinks.tsx
+++ src/app/modules/views/admin/ytLinks/ytLinks.tsx
@@ -108,6 +108,7 @@ const AdminYtLink = () => {
 				<tbody>
 				{ytItem?.map(({name, createdAt, location, _id}) => (
 						<tr
+						key={name}
 						className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
 						<td className="w-4 p-4">
 							<div className="flex items-center">
